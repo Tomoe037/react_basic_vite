6a4f4293cc40fffe88e72b33162a883f3c4e839a
Tomoe030703 <tieuotaku046@gmail.com>
2024-07-09 14:39:46 +0700

    vd31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,14 +1,23 @@
 import './style.css';
 const MyComponent = () => {
+    // const xinchao = "hello";
+    // const xinchao = 20;
+    // const xinchao = true;
+    // const xinchao = undefined;
+    // const xinchao = [1,2,3];
+    const xinchao = {
+        name: "hoidanit",
+        age: "20",
+    };
     return (
         <>
-              <div>hello my husband</div>
-              <div className="child"
-                    style={{borderRadius:" 10px"}}
-              >child</div>
-
+            <div>{JSON.stringify(xinchao)} my husband</div>
+            <div>{console.log("Eric")}</div>
+            <div className="child"
+                style={{ borderRadius: " 10px" }}
+            >child</div>
         </>
     );
-  }
+}
 
-  export default MyComponent;
+export default MyComponent;
