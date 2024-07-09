a7a42a9141d2c6c2874b6752bd4084149334a350
Tomoe030703 <tieuotaku046@gmail.com>
2024-07-09 19:48:52 +0700

    vd33 bai tap component

---
+++ src/components/todo/TodoData.jsx
@@ -0,0 +1,12 @@
+const TodoData = () => {
+    return (
+        <>
+            <div className="todo-data">
+                <div>do it</div>
+                <div>watching tv</div>
+            </div>
+        </>
+    );
+}
+
+export default TodoData;
---
+++ src/components/todo/TodoNew.jsx
@@ -0,0 +1,13 @@
+
+const TodoNew = () => {
+    return (
+        <>
+        <div className="todo-add">
+            <input id="input" type="text" placeholder='Enter your task' />
+            <button className="btnAdd">Add</button>
+        </div>
+    </>
+    )
+}
+
+export default TodoNew;
