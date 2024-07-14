e6847afb904d08ec0456a817c47e0fb1984e3c13
Tomoe030703 <tieuotaku046@gmail.com>
2024-07-14 16:19:15 +0700

    vd48 nested router vs outlet

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import reactLogo from './assets/react.svg'
 import { useState } from 'react'
 import Header from './components/layout/header'
 import Footer from './components/layout/footer'
+import { Outlet } from 'react-router-dom'
 
 
 
@@ -56,6 +57,7 @@ const App = () => {
           />}
 
       </div>
+      <Outlet/>
       <Footer/>
     </>
   )
