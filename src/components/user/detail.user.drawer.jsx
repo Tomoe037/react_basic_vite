a31e55675f8ba823e214f319493b46d53c4695e9
Tomoe030703 <tieuotaku046@gmail.com>
2024-07-29 03:35:57 +0700

    vd 82 upload image with input file

--- src/components/user/detail.user.drawer.jsx
+++ src/components/user/detail.user.drawer.jsx
@@ -8,6 +8,7 @@ const DetailUserDrawer = (props) => {
     return (
         <>
             <Drawer
+                width={"30vw"}
                 title="Detail User"
                 open={isOpenDetail}
                 onClose={() => { 
@@ -22,6 +23,29 @@ const DetailUserDrawer = (props) => {
                     <p>Full Name : {dataDetail.fullName}</p>
                     <p>Email : {dataDetail.email}</p>
                     <p>Phone : {dataDetail.phone}</p>
+                    <div>
+                        <img
+                        width={"240px"}
+                        height={"240px"}
+                        src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${dataDetail.avatar}`}
+                        ></img>
+                    </div>
+                    <div>
+                        <label htmlFor="btnUploadImage"
+                        style={{
+                            display : "block",
+                            background : "#6CB4EE" ,
+                            padding: "5px",
+                            color: "white",
+                            cursor: "pointer",
+                            width: "100px",
+                            height: "fit-content",
+                            borderRadius: "5px",
+
+                        }}
+                        > Upload Image</label>
+                        <input type="file" id='btnUploadImage' hidden />
+                    </div>
                     </>
                     :
                     <p>Không có dữ liệu người dùng </p>
